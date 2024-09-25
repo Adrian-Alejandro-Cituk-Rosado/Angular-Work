93228e4a56cff4defc515dfe237d233f4c77eae4
Adrian Cituk <adrian.citukrosado@cbtis72.edu.mx>
2024-09-25 09:50:41 -0500

    Cambios

--- src/app/counter/components/counter/counter.component.ts
+++ src/app/counter/components/counter/counter.component.ts
@@ -5,11 +5,13 @@ import { Component } from '@angular/core';
   selector: 'app-counter',
   template: `
   <p>Counter: {{counter}}</p>
-<button (click)="increaseBy(1)">Incremento</button>
-
-<button (click)="decreaseBy(1)">Decremento</button>
+<button (click)="increaseBy(1)">+1</button>
 
 <button (click)="resetCounter()">Reiniciar</button>
+
+<button (click)="decreaseBy(1)">-1</button>
+
+
 
   `
 })
